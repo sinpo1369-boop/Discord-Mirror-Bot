99d2800f75c4b8a3bd9c19fdb60909e0a0f9e6cb
sinpo1369-boop <sinpo1369@gmail.com>
2025-08-19 12:07:26 +0700

    Add files via upload

---
+++ BotDiscord.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+import os
+
+# Ambil token dari environment variable (AMAN untuk hosting)
+TOKEN = os.getenv("Your_Bot_Token")
+
+SOURCE_CHANNEL_ID = 1406882509903499284
+TARGET_CHANNEL_ID = 1392575073562394695
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Bot sudah login sebagai {bot.user}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.channel.id == SOURCE_CHANNEL_ID:
+        target_channel = bot.get_channel(TARGET_CHANNEL_ID)
+        if target_channel:
+            content = f"**{message.author.display_name}:** {message.content}"
+            if message.attachments:
+                files = [await att.to_file() for att in message.attachments]
+                await target_channel.send(content, files=files)
+            else:
+                await target_channel.send(content)
+
+    await bot.process_commands(message)
+
+bot.run(TOKEN)
